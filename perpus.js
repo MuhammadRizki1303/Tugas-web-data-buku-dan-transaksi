a52a2f989862a0de0a3d925d900dcd65aaa8f8a1
MuhammadRizki1303 <muhammad.rizki1303@gmail.com>
2023-03-30 15:09:35 +0700

    rizki

--- perpus.js
+++ perpus.js
@@ -1,20 +1,23 @@
-function formdaftar(){
+function formdaftar() {
     idanggota = document.getElementById('idanggota');
     nama = document.getElementById('name');
-    if(idanggota.value==''){
+    if (idanggota.value == '') {
         alert('Id tidak boleh kosong')
         idanggota.focus();
         return false;
-    }
-    else if(nama.value==''){
+    } else if (idanggota.value <= 5) {
+        alert('Id tidak boleh kurang dari 5')
+        idanggota.focus();
+        return false;
+    } else if (nama.value == '') {
         alert('nama tidak boleh kosong');
         nama.focus();
         return false;
-    }else if(nama.value.length <= 3){
+    } else if (nama.value.length <= 3) {
         alert('Minimal 3 Karakter');
         nama.focus();
         return false;
-    }else{
+    } else {
         alert('Data Tersimpan')
         return true;
     }
